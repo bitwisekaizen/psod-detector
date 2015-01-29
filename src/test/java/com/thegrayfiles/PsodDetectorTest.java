513d0d00de7de0d8a00f0beadf3efe4cc3f819c9
Gavin Gray <gavin@infinio.com>
2015-01-28 22:38:56 -0500

    Wire up spring boot.

--- src/test/java/com/thegrayfiles/PsodDetectorTest.java
+++ src/test/java/com/thegrayfiles/PsodDetectorTest.java
@@ -1,10 +1,25 @@
 package com.thegrayfiles;
 
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+// See http://stackoverflow.com/questions/25537436/integration-testing-a-spring-boot-web-app-with-testng
+@TestExecutionListeners(inheritListeners = false, listeners = {
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class })
 @Test
-public class PsodDetectorTest {
+public class PsodDetectorTest extends AbstractTestNGSpringContextTests {
 
     public void canDetectPsod() {
         Assert.fail("fail");
