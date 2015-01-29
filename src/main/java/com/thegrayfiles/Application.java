1e1b051c1c652bd204760aa718b9d2dd83fce2f3
Gavin Gray <gavin@infinio.com>
2015-01-29 12:32:37 -0500

    Build an executable jar.

--- src/main/java/com/thegrayfiles/Application.java
+++ src/main/java/com/thegrayfiles/Application.java
@@ -2,6 +2,9 @@ package com.thegrayfiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        //factory.setPort(9000);
+        //factory.setSessionTimeout(10, TimeUnit.MINUTES);
+        //factory.addErrorPages(new ErrorPage(HttpStatus.404, "/notfound.html"));
+        return factory;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/thegrayfiles/PsodController.java
+++ src/main/java/com/thegrayfiles/PsodController.java
@@ -1,14 +1,17 @@
 package com.thegrayfiles;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PsodController {
 
-    @RequestMapping("/psod")
-    private Psod psod(@RequestParam String vm) {
+    @RequestMapping
+    public Psod psod(@RequestParam String vm) {
         return new Psod(vm);
     }
 }
