fd3c0129662573f7a58f5e94073fd7dd8247b12f
Gavin Gray <gavin@infinio.com>
2015-01-28 22:17:30 -0500

    First failing test.

---
+++ src/test/java/com/thegrayfiles/PsodDetectorTest.java
@@ -0,0 +1,12 @@
+package com.thegrayfiles;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class PsodDetectorTest {
+
+    public void canDetectPsod() {
+        Assert.fail("fail");
+    }
+}
